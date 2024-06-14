6c73ef3b6d11cb09cbadd3f9f834728ba0520710
seanbsu <seancalkins@u.boisestate.edu>
2024-06-14 10:17:30 -0600

    2.1 p[roject created and 3.1-3.3 project created

---
+++ 2.1_TempConverter/TempConverter/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+const int BASE =32;
+const double CONVERSION_FACTOR = 5.0 / 9.0;
+
+double farenheitTemp = 98.6;
+double celciusTemp;
+		
+celciusTemp = (farenheitTemp-BASE) * CONVERSION_FACTOR;
+Console.WriteLine("Celcius Temperature:" + celciusTemp);
+Console.WriteLine("Fahrenheit Temperature: " +  farenheitTemp);
