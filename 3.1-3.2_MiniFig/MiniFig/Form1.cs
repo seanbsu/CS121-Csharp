8d5bff7f6ae54939c486e1005225ade0c06aee65
seanbsu <seancalkins@u.boisestate.edu>
2024-06-22 09:04:04 -0600

    create new minifig and add it to form to show on screen

--- 3.1-3.2_MiniFig/MiniFig/Form1.cs
+++ 3.1-3.2_MiniFig/MiniFig/Form1.cs
@@ -6,4 +6,15 @@ public partial class Form1 : Form
     {
         InitializeComponent();
     }
+     protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics canvas = e.Graphics;
+            int midX = this.ClientSize.Width / 2;
+            int topY = 10; // Arbitrary top margin
+
+            // Create a new MiniFig object and draw it
+            MiniFig miniFig = new MiniFig(canvas, midX, topY);
+            miniFig.draw();
+        }
 }
