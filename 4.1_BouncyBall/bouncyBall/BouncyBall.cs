b552bcaefad0befde3aa4a44ba13aceb69b4d5b4
seanbsu <seancalkins@u.boisestate.edu>
2024-06-24 11:47:50 -0600

    base assignment

--- 4.1_BouncyBall/bouncyBall/BouncyBall.cs
+++ 4.1_BouncyBall/bouncyBall/BouncyBall.cs
@@ -13,5 +13,75 @@ public class BouncyBall : Panel
 
 	private int radius = 10;            // ball radius
 
-    
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            
+            int width = this.ClientSize.Width;
+            int height = this.ClientSize.Height;
+            // Clear canvas
+            using(Brush canvasBrush = new SolidBrush(this.BackColor))
+            {
+                g.FillRectangle(canvasBrush,0,0,width,height);
+            }
+            // Calculate new x anchor point value
+            x += xDelta;
+            // TODO: Add conditional statements to keep ball in bounds
+            //       in the x axis (e.g. left and right edges)
+
+
+            // Calculate new y anchor point value
+            y += yDelta;
+            // TODO: Add conditional statements to keep ball in bounds
+            //       in the y axis (e.g. top and bottom edges)
+
+
+            
+            // Paint the ball at the calculated anchor point
+            using(Brush ballBrush = new SolidBrush(color))
+            {
+                g.FillEllipse(ballBrush,x - radius, y - radius, 2 * radius, 2 * radius);
+            }
+        }
+
+        public BouncyBall()
+        {
+            this.BackColor =Color.White;
+            this.Size = new Size(INIT_WIDTH,INIT_HEIGHT);
+            this.DoubleBuffered = true;
+            
+            // Instantiate instance variable for reuse in OnPaint()
+            random = new Random();
+
+            // Initialize ball color
+		    // TODO: replace with random starting color.
+            color = Color.Red;
+
+            // Initialize ball anchor point within panel bounds
+            // TODO: replace centered starting point with a random
+            // position anywhere in-bounds - the ball should never
+            // extend out of bounds, so you'll need to take RADIUS
+            // into account. Use INIT_WIDTH and INIT_HEIGHT as the
+            // screen width and height.
+            x = INIT_WIDTH/2;
+            y = INIT_HEIGHT/2;
+
+            // Initialize deltas for x and y
+            xDelta = 5;
+            yDelta = 5;
+            
+             System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            timer.Interval = DELAY;
+            timer.Tick += new EventHandler(OnTick);
+            timer.Start();
+            
+        }
+
+         private void OnTick(object sender, EventArgs e)
+        {
+            this.Invalidate();// Triggers the Paint event
+        }
+
+
 }
--- 4.1_BouncyBall/bouncyBall/Form1.cs
+++ 4.1_BouncyBall/bouncyBall/Form1.cs
@@ -5,5 +5,7 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+        BouncyBall bBall = new BouncyBall();
+        this.Controls.Add(bBall);
     }
 }
