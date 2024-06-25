cfca4b4ae899bf4be467f2a4997604c2778e6b10
seanbsu <seancalkins@u.boisestate.edu>
2024-06-25 11:26:20 -0600

    string comparison class, unlike java '==' in c# is overloaded to check for reference equals and value equality so it passes where java would fail

---
+++ 4.2_CompareAndSwitch/StringComp/StringComparison.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace StingComp;
+public class StringComparison
+{
+   static void Main()
+   {
+    const string CODE_WORD = "peanut";
+    // Compare constant to variable with the same value
+    string myWord = "peanut";
+    if(CODE_WORD == myWord)
+    {
+        Console.WriteLine("Access Granted!");
+    }
+    else
+    {
+        Console.WriteLine("Access Denied!");
+    }
+    // Now use readline to read the same word from the user and compare
+    Console.WriteLine("Enter the code word: ");
+    string? input = Console.ReadLine();
+
+    if(input == null)
+    {
+        Console.WriteLine("Access denied!");
+    }
+    else if (input == CODE_WORD)
+    {
+        Console.WriteLine("Access granted!");
+    }
+    else
+    {
+        Console.WriteLine("Access denied!");
+    }
+   }
+}
