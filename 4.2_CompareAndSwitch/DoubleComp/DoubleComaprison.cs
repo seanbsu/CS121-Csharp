aafbc209f7a9dc69ab9b53b7cddab25c447d07a9
seanbsu <seancalkins@u.boisestate.edu>
2024-06-25 11:00:37 -0600

    double comparison class

---
+++ 4.2_CompareAndSwitch/DoubleComp/DoubleComaprison.cs
@@ -0,0 +1,17 @@
+﻿namespace DoubleComp;
+public class DoubleComparison
+{
+    static void Main(){
+        const double TOLERANCE = 0.0000000000000001;
+        double result = 1.0 - 0.9;
+		double expected = 0.1;
+		if(result == expected)
+		{
+			Console.WriteLine("They are equal!");
+		}
+		else
+		{
+			Console.WriteLine("They are not equal.");
+		}
+    }
+}
