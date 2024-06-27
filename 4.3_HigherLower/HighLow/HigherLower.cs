540719f03e115e1b3ee9cf643e543682b174ae7f
seanbsu <seancalkins@u.boisestate.edu>
2024-06-27 09:05:17 -0600

    HigherLower init

---
+++ 4.3_HigherLower/HighLow/HigherLower.cs
@@ -0,0 +1,28 @@
+﻿namespace HighLow;
+public class HigherLower
+{
+    static void Main()
+    {
+        const int MAX =10;
+        int answer;
+        int guess;
+        Random random = new Random();
+        answer = random.Next(MAX)+1;
+        Console.Write("I'm thinking of a number between 1 and " + MAX + ". ");
+        Console.Write("Guess what it is: ");
+
+        guess = int.Parse(Console.ReadLine());
+
+        if(guess == answer)
+        {
+            Console.WriteLine("You got it! Good guessing!");
+        }
+        else
+        {		
+		    Console.WriteLine("That is not correct, sorry.");
+			Console.WriteLine("The number was " + answer + ".");
+        }
+        
+        Console.WriteLine("Game over. Goodbye!");
+    }
+}
